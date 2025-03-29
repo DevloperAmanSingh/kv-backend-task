7f091ea747dec20dfdb6eafe1dd2eb6a565e698c
DevloperAmanSingh <amansinghneh211205@gmail.com>
2025-03-29 12:37:30 +0530

    removed config file

--- flask_auth/app/__init__.py
+++ flask_auth/app/__init__.py
@@ -3,7 +3,6 @@ from app.routes import auth_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("app.config.Config")
     app.register_blueprint(auth_blueprint)
 
     return app
